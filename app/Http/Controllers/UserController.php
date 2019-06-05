10644d8522e3a65b7fd77c2883274afff82cbe2e
= <=>
2019-06-05 12:34:18 -0500

    Controlles, no terminado

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,7 +8,7 @@ class UserController extends Controller
 {
     public function index()
     {
-    	return 'Usuarios';
+    	return view('users');
     }
 
     public function show($id)
---
+++ resources/views/users.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inicio</title>
+</head>
+<body>
+
+	<h1>Bievenidos</h1>
+
+</body>
+</html>
