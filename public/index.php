ca084581a220bdb9043d83bf0cf92c85fffd292a
= <=>
2019-06-05 10:39:23 -0500

    Verificación de Ruta Index

--- public/index.php
+++ public/index.php
@@ -1,11 +1,7 @@
 <?php
 
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
- */
+die($_SERVER['REQUEST_URI']);
+
 
 define('LARAVEL_START', microtime(true));
 
