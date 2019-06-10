0605374d428ffb640c7a3f2bad4ed3084d84ba7b
sebasroldanm <sebasroldanm@gmail.com>
2019-06-09 19:40:14 -0500

    Arreglo

---
+++ resources/views/users.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inicio</title>
+</head>
+<body>
+	
+	<h1>{{ $title }}</h1>
+
+	<hr>
+
+	<ul>
+
+		@forelse ($users as $user)
+
+			<li>{{ $user }}</li>
+
+		@empty
+
+			<li>No hay usuarios disponibles</li>
+
+		@endforelse
+
+	</ul>
+
+</body>
+</html>
