daca6cffefbd2dd7661e12e29c46b9621cd199e5
sebasroldanm <sebasroldanm@gmail.com>
2019-06-11 10:12:10 -0500

    Creación de Descripción (FK)

--- database/migrations/2014_06_11_143514_create_description_table.php
+++ database/migrations/2014_06_11_143514_create_description_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateDescriptionsTable extends Migration
+class CreateDescriptionTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,11 @@ class CreateDescriptionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('descriptions', function (Blueprint $table) {
+        Schema::create('description', function (Blueprint $table) {
             $table->bigIncrements('id');
+
+            $table->string('title');
+
             $table->timestamps();
         });
     }
@@ -26,6 +29,6 @@ class CreateDescriptionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('descriptions');
+        Schema::dropIfExists('description');
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,10 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+
+            $table->integer('description_id')->unsigned()->nullable();
+            $table->foreign('description_id')->references('id')->on('description');
+
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
