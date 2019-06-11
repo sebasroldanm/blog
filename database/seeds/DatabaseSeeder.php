15ff6f242e0a2a2187227aace8b3698b28b05c01
= <=>
2019-06-11 11:29:42 -0500

    Error Funcion

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -21,16 +21,16 @@ class DatabaseSeeder extends Seeder
             $this->call(DescriptionSeeder::class);
             $this->call(UserSeeder::class);
         }
+    }
 
-        protected function truncateTables(array $tables)
-        {
-            DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
+    protected function truncateTables(array $tables)
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
 
-            foreach ($tables as $table) {
-                DB::table($table)->truncate();
-            }
-
-            DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+        foreach ($tables as $table) {
+            DB::table($table)->truncate();
         }
+
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
     }
 }
