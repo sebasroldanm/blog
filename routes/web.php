01b6ee0c432ebc9ac0593d28080dffa5f550a7cb
sebasroldanm <sebasroldanm@gmail.com>
2019-06-12 17:51:28 -0500

    Ejecución Auth

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,6 @@ Route::get('/usuario/{id}', 'UserController@show')->where('id', '[0-9]+');
 Route::get('/usuario/nuevo', 'UserController@create');
 
 Route::get('/saludo/{name}/{nickname?}', 'WelcomeController');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
